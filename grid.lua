1bf4ff46435766793cc9f0286644403eabf5f123
piecake <73677929+pie2280@users.noreply.github.com>
2026-08-23 20:31:49 +0300

    update grid.lua

--- grid.lua
+++ grid.lua
@@ -1,5 +1,6 @@
-local function parse(cords)
   local parseTable = {A=1,B=2,C=3,D=4,E=5,F=6,G=7,H=8,I=9,J=10,K=11,L=12,M=13,N=14,O=15,P=16,Q=17,R=18,S=19,T=20}
+  local convertTable ={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T"}
+local function parse(cords)
   local pri1,pri2,sec1,sec2 = string.match(cords,"(%a)(%d+)%s+(%d):(%d)%s*")
   if not pri1 then return nil,nil,3 end
   local x = (parseTable[string.upper(pri1)]-1)*1000+tonumber(sec1)*100+50
@@ -11,7 +12,6 @@ local function convert(x,y)
   if x>20000 or y>10000 or x<0 or y<0 then return nil,5 end
   if x == 20000 then x = 19950 end
   if y == 10000 then y = 9950 end
-  local convertTable ={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T"}
   local pri1 = convertTable[math.floor(x/1000)+1]
   local pri2 = math.floor(y/1000)+1
   local sec1 = math.floor((x%1000)/100)
