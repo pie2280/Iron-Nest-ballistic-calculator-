7670545f66086db1f8a97d581c5dcbb36a585fb5
piecake <73677929+pie2280@users.noreply.github.com>
2026-08-26 14:58:47 +0300

    Create shell.lua

---
+++ program/shell.lua
@@ -0,0 +1,23 @@
+local current_file = debug.getinfo(1, "S").source:sub(2)
+local script_dir = current_file:match("(.*[/\\])") or "./"
+package.path = package.path .. ";" .. script_dir .. "?.lua"
+
+local main = require("main")
+
+print("Iron Nest ballisic calculator")
+
+while true do
+  io.write("INBC-shell> ")
+  io.flush()
+  local cmd = io.read()
+  if cmd=="exit" then break end
+  if cmd~="" then
+    local com,arg = main.translate(cmd)
+    local response = main.execute(com,arg)
+    if type(response)=="table" then
+      print(table.unpack(response))
+    else
+      print(response)
+    end
+  end
+end
